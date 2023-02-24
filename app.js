f7be4618ed5de08d2ba0d7b43c692bed663f55e8
Paul Thomas <paulthomas2107@gmail.com>
2023-02-24 16:54:15 +0000

    More..

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const calendar = document.getElementById('calendar');
+const monthEl = document.getElementById('month');
 
 const months = [
   'January',
@@ -17,6 +18,10 @@ const months = [
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
+const today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+
 const drawBlankCalendar = () => {
   for (let i = 0; i < 35; i++) {
     const day = document.createElement('div');
@@ -24,6 +29,7 @@ const drawBlankCalendar = () => {
 
     const dayText = document.createElement('p');
     dayText.classList.add('day-text');
+    dayText.innerText = days[i % 7];
 
     const dayNumber = document.createElement('p');
     dayNumber.classList.add('day-number');
@@ -42,19 +48,46 @@ const updateCalendar = (month, year, events) => {
   const dayElements = document.querySelectorAll('.day');
   const theFirst = new Date();
   theFirst.setMonth(month);
-  theFirst.setFullYear(year);
+  theFirst.setYear(year);
 
   const theFirstDayOfWeek = theFirst.getDay();
   const monthName = months[month];
-  const monthWithYear = `${year} - ${month}`;
+  const monthWithYear = `${year} - ${monthName}`;
+  monthEl.innerText = monthWithYear;
   const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  let dayCounter = 1;
 
   for (let i = 0; i < dayElements.length; i++) {
     const day = dayElements[i];
-    const dayText = day.querySelector('.day-text');
-    dayText.innerText = days[i % 7];
+
+    const dayNumber = day.querySelector('.day-number');
+    if (i >= theFirstDayOfWeek && dayCounter <= daysInMonth) {
+      const thisDate = new Date(year, month, dayCounter);
+
+      dayNumber.innerText = dayCounter;
+      dayCounter++;
+    } else {
+      dayNumber.innerText = '';
+    }
   }
 };
 
+const previousMonth = () => {
+  if (currentMonth === 0) {
+    currentMonth = 12;
+    currentYear--;
+  }
+  updateCalendar(--currentMonth, currentYear);
+};
+
+const nextMonth = () => {
+  if (currentMonth === 11) {
+    currentMonth = -1;
+    currentYear++;
+  }
+  updateCalendar(++currentMonth, currentYear);
+};
+
 drawBlankCalendar();
-updateCalendar();
+updateCalendar(currentMonth, currentYear);
