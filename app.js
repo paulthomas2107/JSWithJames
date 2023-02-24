1567f18b2c65d64dfc68d42f3ce26465088544d8
Paul Thomas <paulthomas2107@gmail.com>
2023-02-24 15:52:21 +0000

    More

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const calendar = document.getElementById('calendar');
 
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 const drawBlankCalendar = () => {
   for (let i = 0; i < 35; i++) {
     const day = document.createElement('div');
@@ -21,4 +38,23 @@ const drawBlankCalendar = () => {
   }
 };
 
+const updateCalendar = (month, year, events) => {
+  const dayElements = document.querySelectorAll('.day');
+  const theFirst = new Date();
+  theFirst.setMonth(month);
+  theFirst.setFullYear(year);
+
+  const theFirstDayOfWeek = theFirst.getDay();
+  const monthName = months[month];
+  const monthWithYear = `${year} - ${month}`;
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  for (let i = 0; i < dayElements.length; i++) {
+    const day = dayElements[i];
+    const dayText = day.querySelector('.day-text');
+    dayText.innerText = days[i % 7];
+  }
+};
+
 drawBlankCalendar();
+updateCalendar();
